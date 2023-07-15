b98d4ae45c2f13fccc634e6de23f149164cd1d7f
Thiago Weslley <thiagoweslley@yahoo.com>
2023-07-15 15:13:06 -0300

    fix: Corrige incompatibilidade do carrossel

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -7,7 +7,31 @@ const Slider = ({ info, title }) => {
   return (
     <Container>
       <h2>{title}</h2>
-      <Swiper grabCursor slidesPerView={"auto"} spaceBetween={10}>
+      <Swiper
+        grabCursor
+        slidesPerView={"auto"}
+        spaceBetween={10}
+        breakpoints={{
+          270: {
+            slidesPerView: 1
+          },
+          600: {
+            slidesPerView: 2
+          },
+          860: {
+            slidesPerView: 3
+          },
+          1250: {
+            slidesPerView: 4
+          },
+          1700: {
+            slidesPerView: 6
+          },
+          2200: {
+            slidesPerView: 7
+          }
+        }}
+      >
         {info.map((item, index) => (
           <SwiperSlide key={index}>
             <Card item={item} />
